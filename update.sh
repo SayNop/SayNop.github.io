6adb8ec6076f6da9e49cae17d54088f5bde74ef3
SayNop <a2045751@163.com>
2023-03-07 12:11:20 +0800

    update shell script

--- update.sh
+++ update.sh
@@ -3,9 +3,8 @@ if [ "`ls -A $DIRECTORY`" = "" ]; then
     echo "$DIRECTORY is empty, stop upload!!!"
 else
     rm -rf 404.html assets categories detail index.html page tags
-    mv ../blog_by_vuepress/blog/.vuepress/dist/* .
+    mv `$DIRECTORY*` .
     git add --all
     git commit -m "Update"
     git push
 fi
-
