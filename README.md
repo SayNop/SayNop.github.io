a22d9d7c5617fb61030161be0e6c455fa8ec040f
SayNop <a2045751@163.com>
2023-10-13 17:50:46 +0800

    update vuepress2 shell script

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ click [here](https://saynop.github.io/)
 ## update
 - package dist dir in blog project
 - use this script to update and upload blog online
+
+### vuepress1
 ```bash
 ./update.sh
+```
+
+### vuepress2
+```bash
+./update2.sh
 ```
--- update2.sh
+++ update2.sh
@@ -1,8 +1,8 @@
-DIRECTORY='../blog_by_vuepress/blog/.vuepress/dist/'
+DIRECTORY='../blog_by_vuepress_v2/blog/.vuepress/dist/'
 if [ "`ls -A $DIRECTORY`" = "" ]; then
     echo "$DIRECTORY is empty, stop upload!!!"
 else
-    # rm -rf assets categories detail page tags 404.html favicon.ico index.html omega-sign.png sitemap.xml
+    # rm -rf assets categories category detail tag tags 404.html favicon.ico index.html robots.txt search-pro.worker.js
     rm -rf assets categories category detail page tag tags 404.html favicon.ico index.html omega-sign.png sitemap.xml robots.txt search-pro.worker.js
     mv $DIRECTORY* .
     git add --all
